c431a360f390fb7e475e9b833d0bda5c35759e32
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-22 22:26:48 -0300

    primeira aula commitada

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+function showModal(modalId){
+    document.getElementById(ModalId).style.display = 'block';
+}
+
+function hideModal(modalId){
+    document.getElementById(ModalId).style.display = 'none';
+}
