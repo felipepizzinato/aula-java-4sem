d322529d5c1a8df1e333e186d779a147d5f74607
felipepizzinato <felipepizzinato9@gmail.com>
2025-09-05 22:46:47 -0300

    aula2

---
+++ src/main/java/br/com/fiap/spring_mvc/configuration/InternacionalizacaoConfig.java
@@ -0,0 +1,18 @@
+package br.com.fiap.spring_mvc.configuration;
+
+import org.apache.tomcat.util.descriptor.LocalResolver;
+import org.hibernate.Session;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+public class InternacionalizacaoConfig {
+
+    public LocalResolver localResolver(){
+        SessionLocaleResolver lci =
+    }
+
+    @Override
+    public void addInterrceptores(Interrrceptor){
+
+    }
+
+}
